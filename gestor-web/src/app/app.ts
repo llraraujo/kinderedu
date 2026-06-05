c769b0553b315a5864e0f14dd6380c16bed2fa21
llraraujo <lucasregoaraujo@gmail.com>
2026-06-05 16:04:31 -0300

    modificando o app para a primeira tela ser a de login

--- gestor-web/src/app/app.ts
+++ gestor-web/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('gestor-web');
